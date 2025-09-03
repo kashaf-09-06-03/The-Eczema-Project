51f2d3f84a35a2444772dbd094cc9c97fd046dfa
MariamMustafaa <mariam.m.manny@gmail.com>
2025-09-04 01:22:45 +0300

    update project-section

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template , request , redirect , url_for , session
-from database import create_table , add_posts , get_posts , delet_posts ,search
+from database import create_table , add_posts , get_posts , delet_posts ,search ,add_project,get_project,delete_project
 import os
 from werkzeug.utils import secure_filename
 
@@ -37,7 +37,8 @@ def main():
         ps=search(search_query=search_query) 
     else:   
         ps = get_posts()
-    return render_template("index.html" , admin=session.get("is_admin") , POSTS = ps)
+    pr= get_project()
+    return render_template("index.html" , admin=session.get("is_admin") , POSTS = ps ,PROJECT=pr)
 
 
 @app.route("/login" , methods=["GET","POST"])
@@ -57,6 +58,49 @@ def delete(post_id):
         delet_posts(post_id=post_id)
     return redirect(url_for("main"))  
 
+allowed_image_extensions={"png","jpg","jpeg","gif"}
+allowed_audio_extensions={"mp3","mp4","wav","ma4"}
+allowed_pdf_extensions={"pdf"}
+app.config["project_folder"]="static/project_folder"  
+
+# os.makedirs(app.config["project_folder"], exist_ok=True)
+  
+def allowedfile(name,allowed_set):
+    return '.' in name and name.rsplit('.',1)[1].lower() in allowed_set 
+
+@app.route("/addproject" ,methods=["GET" ,"POST"])
+def add_projects():
+    if request.method=="POST" and session["is_admin"]==True:
+        title=request.form["title"]
+        content=request.form["content"]
+        image_path =None
+        audio_path=None 
+        pdf_path = None
+        image = request.files.get("image")
+        if image and allowedfile(image.filename , allowed_image_extensions):
+            safe_image=secure_filename(image.filename)
+            image.save(os.path.join(app.config["project_folder"],safe_image))
+            image_path = f"project_folder/{safe_image}"
+            
+        audio = request.files.get("audio")
+        if audio and allowedfile(audio.filename , allowed_audio_extensions):
+            safe_audio=secure_filename(audio.filename)
+            audio.save(os.path.join(app.config["project_folder"],safe_audio))
+            audio_path = f"project_folder/{safe_audio}"   
+            
+        pdf = request.files.get("pdf")
+        if pdf and allowedfile(pdf.filename , allowed_pdf_extensions):
+            safe_pdf=secure_filename(pdf.filename)
+            pdf.save(os.path.join(app.config["project_folder"],safe_pdf))
+            pdf_path = f"project_folder/{safe_pdf}"               
+        add_project(title=title , Content=content , image=image_path , pdf=pdf_path , audio=audio_path)
+    return redirect(url_for("main"))
+
+@app.route("/deleteProject/<int:post_id>" , methods=["GET","POST"])
+def delete_projects(post_id):
+    if session["is_admin"] == True:
+        delete_project(post_id=post_id)
+    return redirect(url_for("main"))  
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -11,12 +11,20 @@ def create_table():
             ''')
     # cur.execute("DELETE FROM posts") 
     con.commit() 
+    cur.execute("""CREATE TABLE IF NOT EXISTS projects(id INTEGER PRIMARY KEY AUTOINCREMENT ,
+                title TEXT NOT NULL,
+                CONTENT TEXT,
+            image_path TEXT,
+            audio_path TEXT,
+            pdf_path TEXT,
+            time DATETIME DEFAULT CURRENT_TIMESTAMP)""")
+    con.commit()
     con.close()
     
 def add_posts(Title , Content , image=None):
     con =sqlite3.connect("database.db")
     cur=con.cursor()
-    cur.execute( "INSERT INTO posts (title , content ,image)  VALUES (? ,?,?)", (Title , Content,image))
+    cur.execute("INSERT INTO posts (title , content ,image)  VALUES (? ,?,?)", (Title , Content,image))
     con.commit()
     con.close()   
 
@@ -42,3 +50,27 @@ def search(search_query):
     cur.execute("SELECT id, title, content, time FROM posts WHERE title LIKE ? ORDER BY time DESC" ,('%'+search_query+'%' ,))
     result=cur.fetchall()
     return result
+
+
+def add_project(title , Content , image=None ,pdf=None ,audio=None):
+    con =sqlite3.connect("database.db")
+    cur=con.cursor()
+    cur.execute("INSERT INTO projects (title , CONTENT ,image_path ,pdf_path  ,audio_path )  VALUES (? ,?,?,?,?)", (title,Content,image, pdf ,audio))
+    con.commit()
+    con.close()   
+
+def get_project():
+    con=sqlite3.connect("database.db")
+    cur=con.cursor()
+    cur.execute("SELECT id , title , CONTENT , image_path , pdf_path , audio_path, time FROM projects ORDER BY time DESC")
+    project=cur.fetchall()
+    con.commit()
+    con.close()
+    return project 
+
+def delete_project(post_id):
+    con=sqlite3.connect("database.db")
+    cur=con.cursor()
+    cur.execute("DELETE FROM projects WHERE id=?", (post_id,))
+    con.commit()
+    con.close()
